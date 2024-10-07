a44e05ab90e5d66e91f5b4bdc7e44c3a0b2f3fb6
Arkaydeus <arktechdev+ark1@gmail.com>
2024-10-07 15:34:48 +0100

    Remove comments

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,28 +7,8 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function Dashboard() {
-  // const [user, setUser] = useState<User | null>(null)
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged((authUser) => {
-  //     if (authUser) {
-  //       setUser(authUser)
-  //     } else {
-  //       setUser(null)
-  //       router.push('/login')
-  //     }
-  //   })
-
-  //   return () => unsubscribe()
-  // }, [router])
-
-  // if (!user) {
-  //   return <div>Loading...</div>
-  // }
-
   return (
-    <div className='flex flex-col gap-8 justify-center items-center py-2 min-h-screen'>
+    <div className='flex min-h-screen flex-col items-center justify-center gap-8 py-2'>
       <h1 className='mb-6 text-4xl font-bold'>Dashboard (Client-Side)</h1>
 
       <div className='flex flex-row gap-4'>
